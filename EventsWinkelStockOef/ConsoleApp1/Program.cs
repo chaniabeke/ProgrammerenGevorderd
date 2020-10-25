e25a2e7d864c2fc39601212a598cccbd186fd9b2
Chania Beké <chaniabeke@hotmail.com>
2020-10-25 17:31:00 +0100

    fix events volgorde

--- EventsWinkelStockOef/ConsoleApp1/Program.cs
+++ EventsWinkelStockOef/ConsoleApp1/Program.cs
@@ -11,9 +11,11 @@ namespace ConsoleApp1
             Sales s = new Sales();
             Stockbeheer st = new Stockbeheer();
             GrootHandelaar g = new GrootHandelaar();
+
+            st.ToonStock();
             w.WinkelVerkoop += s.OnWinkelVerkoop;
-            w.WinkelVerkoop += st.OnWinkelVerkoop;
             st.StockWijziging += g.OnStockWijziging;
+            w.WinkelVerkoop += st.OnWinkelVerkoop;
 
             w.VerkoopProduct(new Bestelling(ProductType.Dubbel, 50, 25, "Moerbeekstraat 25 - Geraadsbergen"));
             w.VerkoopProduct(new Bestelling(ProductType.Pils, 50, 25, "Moerbeekstraat 25 - Geraadsbergen"));
